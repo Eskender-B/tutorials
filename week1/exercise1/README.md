5616ed9e96efa788dd4035d32aa45578f3ca7f41
Eskender <eskenderbesrat@gmail.com>
2021-05-23 20:43:25 +0300

    Add exercise1_1 template

---
+++ week1/exercise1/README.md
@@ -0,0 +1,10 @@
+# Exercise 1
+* Simple C code and Standard C Library
+
+## Exercise1_1
+* Write simple C code and Use Standard C Library
+* Compile with gcc
+```gcc hello.c -o hello```
+* Run
+```./hello```
+
---
+++ week1/exercise1/hello.c
@@ -0,0 +1,19 @@
+// TODO 1: Add stdio library
+//	- #include <stdio.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* TODO 2:
+	1. Read from standard input
+		- char name[10];
+		- printf("Name: ");
+		- scanf("%s", name);
+	2. Print to standard output 
+		- printf("Hello %s\n", name);
+	*/
+
+
+	return 0;
+}
