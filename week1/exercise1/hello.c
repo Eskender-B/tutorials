4724be016aa7d2146b7fd7d6022c6b1d8779dde8
Eskender <eskenderbesrat@gmail.com>
2021-05-24 01:01:40 +0300

    Add exercise1_2 template

--- week1/exercise1/hello.c
+++ week1/exercise1/hello.c
@@ -1,24 +1,64 @@
-// TODO 1: Add stdio library
-//	- #include <stdio.h>
-#include <stdio.h>
+// TODO 1: Add unistd.h header for read()/write()
+//	- #include <unistd.h>
+
+
+void myScanLine(char* line){
+	size_t sz;
+	int found = 0;
+	int cnt = 0;
+
+	char buff[100];
+
+	while(!found){
+		/*
+		// TODO 2: use read() low level function to read from standard input
+			- standard input has a file descriptor of 0
+			- size_t read (int fd, void* buf, size_t cnt);
+		*/
+
+
+		for (int i = 0; i < sz; ++i)
+		{
+			if (buff[i] != '\n') line[cnt++] = buff[i];
+			else {found = 1; break;} 
+		}
+		
+	}
+	line[cnt] = '\0';
+}
+
+void myPrintLine(char* string){
+	size_t cnt = 0;
+	char buff[100];
+	while(string[cnt] != '\0'){
+		buff[cnt] = string[cnt];
+		cnt++;
+	}
+	buff[cnt++] = '\n';
+
+	/* TODO 3: Use write() low level function to write to standart output
+		- standard output has a file descriptor of 1
+		- size_t write (int fd, void* buf, size_t cnt);
+
+	*/
+
+
+}
 
 
 
 int main(int argc, char const *argv[])
 {
-	/* TODO 2:
-	1. Read from standard input
-		- char name[10];
-		- printf("Name: ");
-		- scanf("%s", name);
-	2. Print to standard output 
-		- printf("Hello %s\n", name);
-	*/
 
 	char name[10];
-	printf("Name: ");
-	scanf("%s", name);
-	printf("Hello %s\n", name);
+	
+
+	char prompt[] = "Name:";
+	//char prompt[] = {'N', 'a', 'm', 'e', ':', '\0'};
+	myPrintLine(prompt);
+	myScanLine(name);
+	myPrintLine(name);
+
 
 	return 0;
 }
